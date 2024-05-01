a688e095b92b7dac9931a993c74ee666224c55c4
Ayobami <oludemiayobami@gmail.com>
2024-05-01 06:26:15 +0100

    fix(checker): Fix checker error

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -3,6 +3,26 @@
 
 
 def top_students(mongo_collection):
+    """
+    Retrieves schools with their average topic scores
+        from a MongoDB collection.
+
+    Params:
+      mongo_collection (pymongo.collection.Collection): A reference
+        to a MongoDB collection containing
+        school data with a 'topics' field containing score information.
+
+    Returns:
+      cursor: A pymongo cursor object containing
+      the filtered and sorted school documents.
+             Each document includes `_id` (if available), `name`, and
+             a calculated `averageScore` field representing
+             the average score within the `topics` list.
+
+    Raises:
+      pymongo.errors.PyMongoError: If an error occurs while
+      interacting with the MongoDB collection.
+    """
     return mongo_collection.find(
         {},
         {
