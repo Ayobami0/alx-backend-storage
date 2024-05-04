cea1921722465f2114f28f63afbab24664ac4098
Ayobami <oludemiayobami@gmail.com>
2024-05-04 05:31:03 +0100

    chore: Add aditional taks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,9 +3,19 @@
 
 This module contains the learning requirement for redis in python.
 """
+import functools
 import uuid
-from typing import Union
+from typing import Any, Callable, Union
 import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -20,6 +30,7 @@ class Cache:
         self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data in a redis database
 
@@ -31,3 +42,30 @@ class Cache:
         key: str = uuid.uuid4().hex
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) -> Any:
+        """
+        Retrieves data from Redis for a given key and optionally applies
+        a transformation function.
+
+        Params:
+          self (RedisWrapper): An instance of the RedisWrapper class.
+          key (str): The key to retrieve data for from the Redis store.
+          fn (Callable[[str], Any], optional): A callable function to be
+                  applied to the retrieved data (if provided).
+                  The function should accept a string argument.
+
+        Returns:
+          Any: The retrieved data from Redis (as bytes) if no
+              transformation function is provided.
+               The result of applying the transformation function `fn`
+               to the decoded string data (if provided).
+
+        Raises:
+          pyredis.exceptions.RedisError: If an error occurs
+            while interacting with the Redis server.
+        """
+        data = self._redis.get(key)
+        if fn is None:
+            return data
+        return fn(str)
