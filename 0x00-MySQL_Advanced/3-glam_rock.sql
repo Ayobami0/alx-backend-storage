8e3e206a8a27f1516c7e6b4c41cef9dbba1835e3
Ayobami <oludemiayobami@gmail.com>
2024-07-03 14:09:33 +0100

    commit: commit

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- list bands with Glam rock as their main style
+
+SELECT 
+	band_name, COALESCE(split, 2022) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
